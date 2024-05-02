fc7d9641b644ecde94832f2d0ead6d3809754c8f
ngure1 <ngugijoseph700@gmail.com>
2024-05-02 21:37:24 +0300

    feat(auth) : add making requests on signup page modify some form fields to match fields in Karume's status

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -4,22 +4,39 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod'; // Import Zod
 import { zodResolver } from '@hookform/resolvers/zod';
 import InputField from "@/components/ui/InputField"
+import { useUsersMutation } from '@/redux/features/users/usersApiSlice';
+
 
 //Should be imported from the types file.
 type User = {
-    name: string,
+    first_name: string,
+    last_name: string,
     email: string,
     password: string,
+    re_password: string,
 }
+//initialising useUsersMutation 
+const [registerUser , {isLoading}] = useUsersMutation()
 
 //Define the schema for the form
-const userSchema = z.object({
-    name: z.string().min(2).max(50),
-    email: z.string().email(),
-    password: z.string()
-        .min(6)
-        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, 'Password must contain at least one uppercase letter and one digit'),
-});
+const userSchema = z
+	.object({
+		frist_name: z.string().min(2).max(50),
+		last_name: z.string().min(2).max(50),
+		email: z.string().email(),
+		password: z
+			.string()
+			.min(6)
+			.regex(
+				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/,
+				"Password must contain at least one uppercase letter and one digit"
+			),
+		re_password: z.string(),
+	})
+	.refine((data) => data.password === data.re_password, {
+		message: "Passwords don't match",
+		path: ["re_password"],
+	});
 
 
 const SignUpPage = () => {
@@ -32,16 +49,23 @@ const SignUpPage = () => {
     const onSubmit = async (data: User) => {
         console.log(data);
         // Submit the data to your API or perform any other action
+        registerUser(data).unwrap()
+        .then(
+            // todo add toast component (redirect to email for activation)
+        )
+        .catch( error => console.log(error))
     };
 
     return (
         <div>
             <h1>Sign Up</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <InputField label="Name" id="name" register={register("name")} error={errors.name} />
+                <InputField label="Frist Name" id="first_name" register={register("first_name")} error={errors.first_name} />
+                <InputField label="Last Name" id="last_name" register={register("last_name")} error={errors.last_name} />
                 <InputField label="Email" id="email" register={register("email")} error={errors.email} />
                 <InputField label="Password" id="password" register={register("password")} error={errors.password} />
-                <button type="submit">Sign Up</button>
+                <InputField label="Confirm Password" id="re_password" register={register("password")} error={errors.password} />
+                <button type="submit" disabled={isLoading}>Sign Up</button>
             </form>
         </div>
     );
