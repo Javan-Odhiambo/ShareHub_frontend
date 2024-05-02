0c242027695072d213f924bda066964205ff7b8a
ngure1 <ngugijoseph700@gmail.com>
2024-05-03 01:13:51 +0300

    fix(redux) : fix signup form fields

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -15,13 +15,12 @@ type User = {
     password: string,
     re_password: string,
 }
-//initialising useUsersMutation 
-const [registerUser , {isLoading}] = useUsersMutation()
+
 
 //Define the schema for the form
 const userSchema = z
 	.object({
-		frist_name: z.string().min(2).max(50),
+		first_name: z.string().min(2).max(50),
 		last_name: z.string().min(2).max(50),
 		email: z.string().email(),
 		password: z
@@ -40,35 +39,76 @@ const userSchema = z
 
 
 const SignUpPage = () => {
+	//initialising useUsersMutation
+	const [registerUser, { isLoading }] = useUsersMutation();
+    
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<User>({
+		resolver: zodResolver(userSchema),
+	});
 
-    const { register, handleSubmit, formState: { errors } } = useForm<User>({
-        resolver: zodResolver(userSchema),
-    });
-
-    //Function that handles submision of validated data
-    const onSubmit = async (data: User) => {
+	//Function that handles submision of validated data
+	const onSubmit = async (data: User) => {
         console.log(data);
-        // Submit the data to your API or perform any other action
-        registerUser(data).unwrap()
-        .then(
-            // todo add toast component (redirect to email for activation)
-        )
-        .catch( error => console.log(error))
-    };
 
-    return (
-        <div>
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <InputField label="Frist Name" id="first_name" register={register("first_name")} error={errors.first_name} />
-                <InputField label="Last Name" id="last_name" register={register("last_name")} error={errors.last_name} />
-                <InputField label="Email" id="email" register={register("email")} error={errors.email} />
-                <InputField label="Password" id="password" register={register("password")} error={errors.password} />
-                <InputField label="Confirm Password" id="re_password" register={register("password")} error={errors.password} />
-                <button type="submit" disabled={isLoading}>Sign Up</button>
-            </form>
-        </div>
-    );
+		// Submit the data to your API or perform any other action
+		registerUser(data)
+			.unwrap()
+			.then((response)=>(
+                // todo add toast component (redirect to email for activation)
+                console.log(`Created successfully !! ${response}`)
+            )
+            )
+			.catch((error) => console.log(error));
+	};
+
+	return (
+		<div>
+			<h1>Sign Up</h1>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<InputField
+					label="Frist Name"
+					id="first_name"
+					register={register("first_name")}
+					error={errors.first_name}
+				/>
+				<InputField
+					label="Last Name"
+					id="last_name"
+					register={register("last_name")}
+					error={errors.last_name}
+				/>
+				<InputField
+					label="Email"
+					id="email"
+					register={register("email")}
+					error={errors.email}
+				/>
+				<InputField
+					label="Password"
+					id="password"
+					register={register("password")}
+					error={errors.password}
+				/>
+				<InputField
+					label="Confirm Password"
+					id="re_password"
+					register={register("re_password")}
+					error={errors.password}
+				/>
+				<button
+					type="submit"
+                    className='border border-red'
+				>
+					Sign Up
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default SignUpPage;
+
