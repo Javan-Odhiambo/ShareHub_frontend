1c68964068679365016eb21a30b9b62f36db79ba
Javan Otieno <javan.o.otieno@gmail.com>
2024-05-21 15:04:19 +0300

    refactor(app): change extract function to return the id as string

--- src/utils/ExtractIdFromUrl.ts
+++ src/utils/ExtractIdFromUrl.ts
@@ -1,11 +1,11 @@
-export function extractIdFromUrl(url: string): number | null {
+export function extractIdFromUrl(url: string): string | null {
 	// Regular expression to match the ID in the URL
 	const regex = /\/(\d+)\/$/;
 	// Use regex.exec() to extract the ID
 	const match = regex.exec(url);
-	// If a match is found, return the ID as a number
+	// If a match is found, return the ID as a string
 	if (match && match[1]) {
-		return parseInt(match[1], 10);
+		return String(match[1]);
 	} else {
 		// If no match is found, return null or handle the error as needed
 		return null;
