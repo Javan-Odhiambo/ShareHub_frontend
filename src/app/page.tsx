c4bc4ab58237d66a7f254667a4ac15adc02a9de4
Javan Otieno <javan.o.otieno@gmail.com>
2024-05-30 01:49:58 +0300

    feat(app): add contact form functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import ThemeChanger from "@/components/ui/ThemeChanger";
+import emailjs from '@emailjs/browser';
 import Image from "next/image";
 import Link from "next/link";
 import {
@@ -17,9 +18,20 @@ import { Input } from "@/components/ui/input";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react";
 
+const NEXT_PUBLIC_EMAIL_TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID
+const NEXT_PUBLIC_EMAIL_SERVICE_ID = process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID
+
+emailjs.init({
+	publicKey: process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY,
+})
 
 export default function LandingPage() {
+	const { toast } = useToast();
+	const [isSendingMessage, setIsSendingMessage] = useState(false);
+
 	// Declare a zod schema for the form with name, email and message fields
 	const schema = z.object({
 		name: z.string().min(3),
@@ -37,7 +49,33 @@ export default function LandingPage() {
 
 	//Handle form submission
 	const onSubmit = async (data: FormValues) => {
-		console.log(data);
+		setIsSendingMessage(true);
+
+		if (!NEXT_PUBLIC_EMAIL_TEMPLATE_ID || !NEXT_PUBLIC_EMAIL_SERVICE_ID) {
+			console.error("Email was not sent!!");
+			return;
+		}			
+
+		emailjs.send(NEXT_PUBLIC_EMAIL_SERVICE_ID, NEXT_PUBLIC_EMAIL_TEMPLATE_ID, {...data})
+
+		emailjs
+			.send(NEXT_PUBLIC_EMAIL_SERVICE_ID, NEXT_PUBLIC_EMAIL_TEMPLATE_ID, {...data})
+			.then(
+				() => {
+					toast({
+						title: "Message sent successfully"
+					});
+					form.reset();
+					setIsSendingMessage(false);
+				},
+				(error) => {
+					toast({
+						title: "Error sending message"
+					});
+					console.log('FAILED...', error.text);
+					setIsSendingMessage(false);
+				},
+			);
 	}
 
 
@@ -193,7 +231,7 @@ export default function LandingPage() {
 								</FormItem>
 							)}
 						/>
-						<Button type="submit">
+						<Button type="submit" disabled={isSendingMessage}>
 							Submit
 						</Button>
 					</form>
